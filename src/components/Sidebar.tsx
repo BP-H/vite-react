e9ca769c0396e6449e76869a16f03d0374178d81
BP-H <blueprinthustler@gmail.com>
2025-08-12 12:41:55 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,19 @@
 // src/components/Sidebar.tsx
-export default function Sidebar({ onOpen }: { onOpen: () => void }) {
+type Props = { onOpen: () => void };
+
+export default function Sidebar({ onOpen }: Props) {
   return (
     <aside className="sidebar">
       <div className="sidebar-inner">
         <div className="brand">Sidebar</div>
         <button className="siri-orb" onClick={onOpen}>Open Portal</button>
 
-        <div className="side-links">
+        <nav className="side-links">
           <div className="section">Profile</div>
           <a>My Worlds</a>
           <a>Following</a>
           <a>Discover</a>
-        </div>
+        </nav>
       </div>
     </aside>
   );
