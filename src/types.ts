a68b3fe789421d0f2c952edf11f0c65dc9f59fde
BP-H <blueprinthustler@gmail.com>
2025-08-12 13:01:08 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,6 @@
 export type Post = {
-  id: string;
-  title: string;
+  id: number;
   author: string;
-  image?: string;
+  title: string;
+  image: string;
 };
-
-export const demoPosts: Post[] = [
-  { id: "1", title: "Prototype Moment", author: "@proto_ai" },
-  { id: "2", title: "Symbolic Feed", author: "@neonfork" },
-  { id: "3", title: "Ocean Study", author: "@superNova_2177" },
-];
